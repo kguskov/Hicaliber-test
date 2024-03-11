c4e9c2d0d57238a19b24c3a3f276103634294876
kguskov <kirill.guskov@gmail.com>
2024-03-11 22:10:37 +0400

    Add SearchScopeTest for testing requests

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -12,7 +12,7 @@ class PropertyController extends Controller
     public function __invoke(PropertyRequest $request): JsonResponse
     {
         // List of all query parameters that SearchScope uses
-        $searchCriteria = ['name', 'bedrooms', 'bathrooms', 'storeys', 'garages', 'price'];
+        $searchCriteria = ['name', 'bedrooms', 'bathrooms', 'storeys', 'garages', 'min_price', 'max_price'];
 
         // Check if the request has any of these parameters
         if ($request->hasAny($searchCriteria)) {
