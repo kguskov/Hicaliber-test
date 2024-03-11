d2a80888e1c11c079ba383a23a4f1c0ba6ffb9d6
kguskov <kirill.guskov@gmail.com>
2024-03-11 22:07:03 +0400

    Add PropertyController logic

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\PropertyRequest;
+use App\Models\Property;
+use Illuminate\Http\JsonResponse;
 
 class PropertyController extends Controller
 {
-    //
+    public function __invoke(PropertyRequest $request): JsonResponse
+    {
+        // List of all query parameters that SearchScope uses
+        $searchCriteria = ['name', 'bedrooms', 'bathrooms', 'storeys', 'garages', 'price'];
+
+        // Check if the request has any of these parameters
+        if ($request->hasAny($searchCriteria)) {
+
+            $properties = Property::search($request)
+                ->paginate();
+        } else {
+            // If not, paginate all properties
+            $properties = Property::paginate();
+        }
+
+        return response()->json(['properties' => $properties]);
+    }
 }
