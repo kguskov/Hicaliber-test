ec301658e963c69b4a3bb6693c27bad1567e9b93
kguskov <kirill.guskov@gmail.com>
2024-03-12 13:58:06 +0400

    Add Tailwind CSS

--- resources/js/Components/Aside.vue
+++ resources/js/Components/Aside.vue
@@ -5,22 +5,17 @@
                 <el-sub-menu index="1">
                     <template #title>
                         <el-icon>
-                            <message/>
+                            <svg class="h-8 w-8 text-gray-500"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <path d="M3 7v11m0 -4h18m0 4v-8a2 2 0 0 0 -2 -2h-8v6" />  <circle cx="7" cy="10" r="1" /></svg>
                         </el-icon>
-                        Navigator One
+                        Bedrooms
                     </template>
-                    <el-menu-item-group>
-                        <template #title>Group 1</template>
-                        <el-menu-item index="1-1">Option 1</el-menu-item>
-                        <el-menu-item index="1-2">Option 2</el-menu-item>
-                    </el-menu-item-group>
-                    <el-menu-item-group title="Group 2">
-                        <el-menu-item index="1-3">Option 3</el-menu-item>
-                    </el-menu-item-group>
-                    <el-sub-menu index="1-4">
-                        <template #title>Option4</template>
-                        <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
-                    </el-sub-menu>
+                    <div class="p-4 mb-2 flex items-center text-sm w-0.5">
+                        <el-radio-group class="pl-4 ml-4">
+                            <el-radio :value="3">3 Bedrooms</el-radio>
+                            <el-radio :value="4">4 Bedrooms</el-radio>
+                            <el-radio :value="5">5 Bedrooms</el-radio>
+                        </el-radio-group>
+                    </div>
                 </el-sub-menu>
                 <el-sub-menu index="2">
                     <template #title>
